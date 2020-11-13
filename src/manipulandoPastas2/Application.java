bcaf3eaa910009c2566ce0aca0750d587b271285
Antony Andrei <andrey_linkin@hotmail.com>
2020-11-13 17:27:35 -0300

    Adicionado segunda parte de como manipular pastas

---
+++ src/manipulandoPastas2/Application.java
@@ -0,0 +1,24 @@
+package manipulandoPastas2;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Enter a file path: ");
+		String strPath = ler.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("Nome do Arquivo: " + path.getName());
+		System.out.println("Nome da Pasta que o arquivo está: " + path.getParent());
+		System.out.println("Diretório completo: " + path.getPath());
+		
+		
+	}
+
+}
