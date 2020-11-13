0776aecab39594297ea90b9b0e385a3cd59e700c
Antony Andrei <andrey_linkin@hotmail.com>
2020-11-13 12:11:05 -0300

    Adicionado Escrita de dados (FileWriter e BufferedWriter

---
+++ src/FileWriterBufferedWriter/Application.java
@@ -0,0 +1,29 @@
+package FileWriterBufferedWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Application {
+
+	public static void main(String[] args) {
+		String[] lines = new String[] {
+				"Bom dia",
+				"Boa Tarde",
+				"Boa noite"
+		};
+		
+		String path = "C:\\Windows\\Temp\\out.txt";
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+	}
+
+}
